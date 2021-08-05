8f609b0aa100d2da5b3b51e64345367724cff34b
brock <brocksmedley@gmail.com>
2021-08-05 16:29:42 -0700

    update flashbots script for EIP-1559

--- src/flashbots.mjs
+++ src/flashbots.mjs
@@ -4,8 +4,8 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import contracts from "./contracts.mjs";
-import { checkSimulation, beepBoop, gasPriceToGwei, ETHER } from "./util.mjs";
-const MINER_REWARD_IN_WEI = ETHER.div(1000).mul(15); // 0.015 ETH
+import { checkSimulation, beepBoop, gasPriceToGwei, GWEI } from "./util.mjs";
+const PRIORITY_GAS_PRICE = GWEI.mul(49);
 const BLOCKS_IN_FUTURE = 2;
 
 // wallets
@@ -25,7 +25,6 @@ const flashbotsProvider = await FlashbotsBundleProvider.create(provider, authSig
 // contracts
 const nimbusTokenContract = new Contract(contracts.nimbusToken.address, contracts.nimbusToken.abi, provider);
 const nimbusGovernanceContract = new Contract(contracts.nimbusGovernance.address, contracts.nimbusGovernance.abi, provider);
-const checkAndSendContract = new Contract(contracts.checkAndSend.address, contracts.checkAndSend.abi, provider);
 
 // print starting ETH balances
 console.log(`VICTIM WALLET\t(${victimAddress}) ${utils.formatEther(await provider.getBalance(victimAddress))} ETH`);
@@ -42,101 +41,95 @@ console.log("NBU Balance", utils.formatEther(nimbusTokenBalance));
 console.log("GNBU Balance", utils.formatEther(nimbusGovernanceBalance));
 
 // how much we should expect to have after unstake
-const expectedNimbusBalance = BigNumber.from("0x39906053ca922b50e0");        // pre-calculated with hardhat // TODO: always re-run right before recovery since this should increase over time)
-const expectedNimbusGovernanceBalance = BigNumber.from("0x175a1794902f028bb4");        // pre-calculated with hardhat // TODO: always re-run right before recovery since this should increase over time)
+const expectedNimbusBalance = BigNumber.from("0x39e84c9ea6cbf663ed");        // pre-calculated with hardhat // TODO: always re-run right before recovery since this should increase over time)
+const expectedNimbusGovernanceBalance = BigNumber.from("0x178fa25abac8108602");        // pre-calculated with hardhat // TODO: always re-run right before recovery since this should increase over time)
 
 const nonce = await provider.getTransactionCount(victimAddress);
+const block = await provider.getBlock("latest");
 console.log("starting nonce", nonce);
+console.log("starting block", block.number);
+console.log("block base gas fee", block.baseFeePerGas);
 
 // transactions: unstake & transfer
-const zeroGasTxs = [
+const sponsoredTransactions = [
     { // unvest NBU tokens
         ...(await nimbusTokenContract.populateTransaction.unvest()),
-        gasPrice: BigNumber.from(0),
-        gasLimit: BigNumber.from(180000),
         nonce: nonce,
     },
     { // unvest GNBU tokens
         ...(await nimbusGovernanceContract.populateTransaction.unvest()),
-        gasPrice: BigNumber.from(0),
-        gasLimit: BigNumber.from(80000),
         nonce: nonce + 1,
     },
     { // transfer NBU tokens to safe address
         ...(await nimbusTokenContract.populateTransaction.transfer(recipientAddress, expectedNimbusBalance)),
-        gasPrice: BigNumber.from(0),
-        gasLimit: BigNumber.from(50000),
         nonce: nonce + 2,
     },
     { // transfer GNBU tokens to safe address
         ...(await nimbusGovernanceContract.populateTransaction.transfer(recipientAddress, expectedNimbusGovernanceBalance)),
-        gasPrice: BigNumber.from(0),
-        gasLimit: BigNumber.from(50000),
         nonce: nonce + 3,
     },
 ];
 
-// build donor transaction
-const checkTargets = [
-    nimbusTokenContract.address,
-    nimbusGovernanceContract.address,
-];
-const checkPayloads = [
-    nimbusTokenContract.interface.encodeFunctionData('availableForTransfer', [recipientAddress]),
-    nimbusGovernanceContract.interface.encodeFunctionData('availableForTransfer', [recipientAddress]),
-];
-const checkMatches = [
-    nimbusTokenContract.interface.encodeFunctionResult('availableForTransfer', [expectedNimbusBalance]),
-    nimbusGovernanceContract.interface.encodeFunctionResult('availableForTransfer', [expectedNimbusGovernanceBalance]),
-];
-const donorTx = {
-    ...(await checkAndSendContract.populateTransaction.check32BytesAndSendMulti(checkTargets, checkPayloads, checkMatches)),
-    value: MINER_REWARD_IN_WEI,
-    gasPrice: BigNumber.from(0),
-    gasLimit: BigNumber.from(400000),
-};
+const gasEstimates = await Promise.all(sponsoredTransactions.map(tx =>
+    provider.estimateGas({
+      ...tx,
+      from: tx.from === undefined ? victimSigner.address : tx.from
+    }))
+);
+console.log("gas estimates", gasEstimates);
+const gasEstimateTotal = gasEstimates.reduce((acc, cur) => acc.add(cur), BigNumber.from(0))
+const gasPrice = PRIORITY_GAS_PRICE.add(block.baseFeePerGas || 0);
 
-// flashbots bundle
-const bundle = [
-    ...zeroGasTxs.map(transaction => {
-        return {
-            transaction,
-            signer: victimSigner,
-        }
-    }),
+const bundleTransactions = [
     {
-        transaction: donorTx,
-        signer: donorSigner,
-    }
+      transaction: {
+        to: victimSigner.address,
+        gasPrice: gasPrice,
+        value: gasEstimateTotal.mul(gasPrice),
+        gasLimit: 21000,
+      },
+      signer: donorSigner,
+    },
+    ...sponsoredTransactions.map((transaction, txNumber) => {
+      return {
+        transaction: {
+          ...transaction,
+          gasPrice: gasPrice,
+          gasLimit: gasEstimates[txNumber],
+        },
+        signer: victimSigner,
+      }
+    })
 ];
-const signedBundle = await flashbotsProvider.signBundle(bundle);
 
-// simulate the bundle
-const gasPrice = await checkSimulation(flashbotsProvider, signedBundle);
-console.log("simulation gasPrice", gasPrice);
+// const signedBundle = await flashbotsProvider.signBundle(bundleTransactions);
+
+// // simulate the bundle
+// const simulationGasPrice = await checkSimulation(flashbotsProvider, signedBundle);
+// console.log("simulation gas price", simulationGasPrice);
 
 // WARNING
 console.log("SENDING TRANSACTION TO FLASHBOTS.");
 await beepBoop(5, "PRESS CTRL-C NOW TO QUIT.");
 
 // send the bundle for real
-provider.on('block', async (blockNumber) => {
-    const gasPrice = await checkSimulation(flashbotsProvider, signedBundle);
-    const targetBlockNumber = blockNumber + BLOCKS_IN_FUTURE;
-    console.log(`Current Block Number: ${blockNumber},   Target Block Number:${targetBlockNumber},   gasPrice: ${gasPriceToGwei(gasPrice)} gwei`)
-    const bundleResponse = await flashbotsProvider.sendBundle(bundle, targetBlockNumber);
-    const bundleResolution = await bundleResponse.wait()
-    if (bundleResolution === FlashbotsBundleResolution.BundleIncluded) {
-        console.log(`Congrats, included in ${targetBlockNumber}`);
-        // print new balances to compare
-        console.log("\n*** Post-exit Balances ***");
-        const newDuckBalance = await duckTokenContract.balanceOf(victimAddress);
-        console.log("DUCK Balance", utils.formatEther(newDuckBalance));
-        process.exit(0);
-    } else if (bundleResolution === FlashbotsBundleResolution.BlockPassedWithoutInclusion) {
-        console.log(`Not included in ${targetBlockNumber}`)
-    } else if (bundleResolution === FlashbotsBundleResolution.AccountNonceTooHigh) {
-        console.log("Nonce too high, bailing")
-        process.exit(1)
-    }
-});
+// provider.on('block', async (blockNumber) => {
+//     const gasPrice = await checkSimulation(flashbotsProvider, signedBundle);
+//     const targetBlockNumber = blockNumber + BLOCKS_IN_FUTURE;
+//     console.log(`Current Block Number: ${blockNumber},   Target Block Number:${targetBlockNumber},   gasPrice: ${gasPriceToGwei(gasPrice)} gwei`)
+//     const bundleResponse = await flashbotsProvider.sendBundle(bundle, targetBlockNumber);
+//     const bundleResolution = await bundleResponse.wait()
+//     if (bundleResolution === FlashbotsBundleResolution.BundleIncluded) {
+//         console.log(`Congrats, included in ${targetBlockNumber}`);
+//         // print new balances to compare
+//         console.log("\n*** Post-exit Balances ***");
+//         const newDuckBalance = await duckTokenContract.balanceOf(victimAddress);
+//         console.log("DUCK Balance", utils.formatEther(newDuckBalance));
+//         process.exit(0);
+//     } else if (bundleResolution === FlashbotsBundleResolution.BlockPassedWithoutInclusion) {
+//         console.log(`Not included in ${targetBlockNumber}`)
+//     } else if (bundleResolution === FlashbotsBundleResolution.AccountNonceTooHigh) {
+//         console.log("Nonce too high, bailing")
+//         process.exit(1)
+//     }
+// });
